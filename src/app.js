76a1adf71b52cc737047935bc454056208ed9acf
Richard <info@monkemedia.co.uk>
2020-07-21 17:03:20 +0100

    Changed port

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+
 const express = require('express')
 const routers = require('./routers/index.js')
 const bodyParser = require('body-parser')
 
-const port = process.env.PORT
+const port = process.env.PORT || 3070
 require('./db/db')
 
 const app = express()
