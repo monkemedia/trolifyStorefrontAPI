09d9a3c5794d7c2f1963ce0f8f06d33f1d2df464
Richard <info@monkemedia.co.uk>
2020-09-03 22:13:41 +0100

    payments is now working

--- src/routers/payments.js
+++ src/routers/payments.js
@@ -2,13 +2,10 @@ const express = require('express')
 const router = express.Router()
 const { auth } = require('../middleware/auth')
 const {
-  createPayment,
-  getPayment
+  createPayment
 } = require('../controller/payments')
 
 // Create new Payment Account
 router.post('/:storeHash/payments', auth, (req, res) => createPayment(req, res))
-// Get payment
-router.get('/:storeHash/payments/:paymentId', auth, (req, res) => getPayment(req, res))
 
 module.exports = router
